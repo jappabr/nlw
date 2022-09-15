27768bc75fe78edd7ffe7f819f655c3569df9b61
jappabr <guilherme.dion@gmail.com>
2022-09-14 21:19:47 -0300

    Initial commit

---
+++ server/src/server.ts
@@ -0,0 +1,26 @@
+import express, { request, response } from 'express'
+const app = express()
+
+app.get('/games', (request, response) =>{
+    return response.json([]);
+});
+
+app.post('/ads', (request, response)=>{
+    return response.status(201).json([]);
+});
+
+app.get('/games/:id/ads', (request, response)=>{
+    return response.json([
+        {id: 1, name: 'Anuncio 1'},
+        {id: 2, name: 'Anuncio 2'},
+        {id: 3, name: 'Anuncio 3'},
+        {id: 4, name: 'Anuncio 4'},
+        {id: 5, name: 'Anuncio 5'},
+    ])
+})
+
+app.get('/ads/:id/discord', (request, response)=>{
+    return response.json([])
+})
+
+app.listen(3333)
