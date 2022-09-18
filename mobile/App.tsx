fe1b612d96f9ea02c40950ab562de50999c94daa
jappabr <guilherme.dion@gmail.com>
2022-09-18 20:23:50 -0300

    Update App.tsx

--- mobile/App.tsx
+++ mobile/App.tsx
@@ -1,27 +1,33 @@
-import { StatusBar } from 'react-native';
-import{
+import { StatusBar } from "react-native";
+import {
   useFonts,
   Inter_400Regular,
   Inter_600SemiBold,
   Inter_700Bold,
   Inter_900Black,
 } from '@expo-google-fonts/inter'
-import { Home } from './src/screens/Home';
-import { Background } from './src/components/Background';
-import { Loading } from './src/components/Loading';
+
+import { Routes } from './src/routes';
+import { Background } from "./src/components/Background";
+import { Loading } from "./src/components/Loading";
 
 export default function App() {
   const [fontsLoaded] = useFonts({
     Inter_400Regular,
     Inter_600SemiBold,
     Inter_700Bold,
-    Inter_900Black
+    Inter_900Black,
   });
 
   return (
     <Background>
-      <StatusBar barStyle="light-content" backgroundColor="transparent" translucent/>
-      {fontsLoaded ? <Home/> : <Loading/>}
+      <StatusBar
+        barStyle="light-content"
+        backgroundColor="transparent"
+        translucent
+      />
+
+      {fontsLoaded ? <Routes /> : <Loading />}
     </Background>
   );
 }
