9422f7fe119c29655bd93b0e86e391bc058b3eb7
jappabr <guilherme.dion@gmail.com>
2022-09-18 20:27:31 -0300

    Update convert-minutes-to-hour-string.ts

--- server/src/utils/convert-minutes-to-hour-string.ts
+++ server/src/utils/convert-minutes-to-hour-string.ts
@@ -1,6 +1,6 @@
-export function convertMinutesToHourString(minutesAmount: number){
-    const hours = Math.floor(minutesAmount/60);
-    const minutes = minutesAmount % 60;
+export function convertMinutesToHourString(minutesAmount: number) {
+  const hour = Math.floor(minutesAmount / 60);
+  const minutes = minutesAmount % 60;
 
-    return `${String(hours).padStart(2, '0')}:${String(minutes).padStart(2, '0')}`;
+  return `${String(hour).padStart(2, '0')}:${String(minutes).padStart(2, '0')}`;
 }
